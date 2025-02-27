82e0dfb2f94a85e6e9961483ca8244d1de851d9f
Arindam Pal <arindampal2000@gmail.com>
2025-02-28 00:41:32 +0530

    added prettier

---
+++ backend/prettier.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    quoteProps: "as-needed",
+    trailingComma: "es5",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always",
+};
+
+export default config;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,32 @@
 import Fastify from "fastify";
 
-const fastify = Fastify({logger: true});
+const fastify = Fastify({ logger: true });
 
-fastify.get("/", async function(request, reply) {
-    return {msg: "Hello, World!"}
+fastify.get("/", async function (request, reply) {
+    return { msg: "Hello, World!" };
 });
 
-fastify.get("/health", async function(request, reply) {
-    return {health: "ok"}
-})
+fastify.get("/health", async function (request, reply) {
+    return { health: "ok" };
+});
 
 async function main() {
     try {
-        fastify.listen({ port: 3000, host: "0.0.0.0" }, function(err, address) {
-            if (err) {
-                fastify.log.error(err);
-                return;
-            }
+        fastify.listen(
+            { port: 3000, host: "0.0.0.0" },
+            function (err, address) {
+                if (err) {
+                    fastify.log.error(err);
+                    return;
+                }
 
-            fastify.log.info(`Server listening on ${address}`);
-        })
+                fastify.log.info(`Server listening on ${address}`);
+            }
+        );
     } catch (err) {
         fastify.log.error(err);
         return;
     }
 }
 
-main().catch(err => console.error(err));
+main().catch((err) => console.error(err));
